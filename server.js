2ddf721ea44c8a22e7cfb035853aba915296df3b
Wenxue Zhao <ballista01@outlook.com>
2021-07-30 09:13:11 -0400

    c5v2

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ passport.serializeUser((id, done) => {
   done(null, user._id);
 });
 passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
     done(null, null);
-  // })
+  })
 })
 
 myDB(async client => {
