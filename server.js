23fea7834bebd81ce11fc44661310d55bc216a33
Wenxue Zhao <ballista01@outlook.com>
2021-07-30 08:20:36 -0400

    c3

--- server.js
+++ server.js
@@ -3,10 +3,25 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 // const pug = require('pug');
 
 const app = express();
 app.set('view engine', 'pug');
+
+// initialize session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true, 
+  cookie: { secure: false },
+}))
+
+// initialize passport
+passport.initialize();
+app.use(passport.initialize());
+passport.session();
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
