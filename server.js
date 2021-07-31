506fc2504636483f976c01e3a3739fc50a158c68
Wenxue Zhao <ballista01@outlook.com>
2021-07-31 09:24:14 -0400

    c6

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
 const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
 // const pug = require('pug');
 
 const app = express();
@@ -36,7 +37,7 @@ myDB(async client => {
   })
 
   // Serialization and deserialization here...
-  passport.serializeUser((id, done) => {
+  passport.serializeUser((user, done) => {
     done(null, user._id);
   });
   passport.deserializeUser((id, done) => {
@@ -45,7 +46,17 @@ myDB(async client => {
     })
   });
 
-
+  passport.use(new LocalStrategy(
+    function (username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user){
+        console.log('User ' + username + 'attempted to log in.');
+        if(err) return done(err);
+        if(!user) return done(null, false);
+        if(password !== user.password) return done(null, false);
+        return done(null, user);
+      });
+    }
+  ))
 
 }).catch(e => {
   app.route('/').get((req, res) => {
