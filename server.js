62631cb43344e3ec9202ffe34dff9563f834b53d
Wenxue Zhao <ballista01@outlook.com>
2021-08-01 18:50:49 -0400

    add user registration

--- server.js
+++ server.js
@@ -34,8 +34,47 @@ myDB(async client => {
       title: 'Connected to Database',
       message: 'Please login',
       showLogin: true,
+      showRegistration: true,
     })
   })
+
+  app.route('/profile').get(ensureAuthenticated, (req, res) => {
+    res.render('pug/profile', {
+      username: req.user.username,
+    });
+  })
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  })
+
+  app.route('/register').post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, function (err, user) {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne({ username: req.body.username, password: req.body.password }, (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              next(null, doc.ops[0]);
+            }
+          });
+        }
+      });
+    },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
+
+  // app.use((req, res, next) => {
+  //   res.status(404).type('text').send('Not Found');
+  // })
 
   // Serialization and deserialization here...
   passport.serializeUser((user, done) => {
@@ -58,6 +97,7 @@ myDB(async client => {
       });
     }
   ));
+
 
 }).catch(e => {
   app.route('/').get((req, res) => {
@@ -82,18 +122,12 @@ function ensureAuthenticated(req, res, next) {
   }
   res.redirect('/');
 }
-app.route('/profile').get(ensureAuthenticated, (req, res) => {
-  res.render('pug/profile', {
-    username: req.user.username,
-  });
-})
-app.route('/logout').get((req, res)=>{
-  req.logout();
-  res.redirect('/');
-})
+
 // app.route('/').get((req, res) => {
 //   res.render('pug/index', { title: 'Hello', message: 'Please login' });
 // });
+
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
